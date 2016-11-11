af381d3859a42b7c7c7dcb4deb4c3c69fdfd29ae
rootcss <shekhar003007@gmail.com>
2016-11-11 14:48:45 +0530

    adds examples

---
+++ examples/example_1.py
@@ -0,0 +1,33 @@
+from simple_spark_lib import SimpleSparkCassandraWorkflow
+import json
+
+cassandra_connection_config = {
+  'host':     '192.168.56.101',
+  'username': 'cassandra',
+  'password': 'cassandra'
+}
+cassandra_config = {
+  'cluster': 'rootCSSCluster',
+  'tables': {
+    'api_events': 'simpl_events_production.api_events'
+  }
+}
+
+workflow = SimpleSparkCassandraWorkflow(appName="Simple Example Worker")
+workflow.setup(cassandra_connection_config, cassandra_config)
+
+query = "SELECT * FROM api_events WHERE event_name = 'UserApprovedEvent'"
+df_payload = workflow.process(query=query)
+
+def mapper(bbb):
+  return (bbb['merchant']['id'], bbb['user']['id'], bbb['user']['first_name'])
+
+zzz = df_payload\
+        .select("payload")\
+        .flatMap(lambda p: p)\
+        .map(lambda a: json.loads(a))
+
+abc1 = zzz\
+          .map(lambda bbb: mapper(bbb))
+
+print abc1.take(10)
---
+++ examples/example_2.py
@@ -0,0 +1,24 @@
+from simple_spark_lib import SimpleSparkContext
+
+cassandra_connection_config = {
+  'host':     '192.168.56.101',
+  'username': 'cassandra',
+  'password': 'cassandra'
+}
+cassandra_config = {
+  'cluster': 'rootCSSCluster',
+  'tables': {
+    'api_events': 'simpl_events_production.api_events',
+    'transactions': 'simpl_events_production.transactions',
+    'api_events2':'cequel_development.api_events',
+  }
+}
+
+ssc = SimpleSparkContext()
+ssc.setConf(cassandra_connection_config=cassandra_connection_config)
+ssc.setContext()
+ssc.setSqlContext()
+ssc.cassandra_sql(cassandra_config=cassandra_config)
+print ssc.execute("SELECT bucket_id, event_name FROM api_events").count()
+print ssc.execute("SELECT * FROM transactions").count()
+print ssc.execute("SELECT * FROM api_events2").count()
